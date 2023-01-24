710c72a6917ca2efd7586ee375fc1bf5fdb9db48
jiwon79 <19-079@ksa.hs.kr>
2023-01-24 17:14:54 +0900

    :card_file_box: Plan Entity 추가

---
+++ src/main/kotlin/com/jiiwon79/motteapp/core/plan/database/PlanEntity.kt
@@ -0,0 +1,20 @@
+package com.jiiwon79.motteapp.core.plan.database
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+
+@Entity(name = "plan")
+class PlanEntity (
+    var date: LocalDateTime,
+    val title: String,
+    val tag: String,
+    val content: String? = null,
+    val location: String? = null,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0L
+}
